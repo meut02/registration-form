5770df11be1e72a580013775a4abbd6638e3b0a5
ut02 <utsavsimplecool@gmail.com>
2023-10-19 12:07:16 +0530

    expense frontend

--- form.js
+++ form.js
@@ -1,4 +1,4 @@
-function savetoLocalStorage(event){
+async function savetoserver(event){
     event.preventDefault();
     const expenseamount=event.target.expenseamount.value
     const description=event.target.description.value
@@ -9,37 +9,63 @@ function savetoLocalStorage(event){
         description,
         category
     }
-    localStorage.setItem(obj.description,JSON.stringify(obj))
-    showuseronscreen(obj)
+    try{
+        const response=await axios.post('http://localhost:5000/Expense/Add-expense',obj)
+        showexpenseonscreen(response.data.newexpensedetails)
+       console.log(response)
+    }
+
+    catch(err){
+        console.log(err)
+
+    }
 }
-function showuseronscreen(obj){
+
+async function refresh(){
+try{
+    const response= await axios.get('http://localhost:5000/Expense/Get-expense')
+
+    for( const expense of response.data.allexpense){
+        showexpenseonscreen(expense)
+    }
+}
+
+catch(err){
+    console.log(err);
+}
+
+}
+
+
+function showexpenseonscreen(expense){
     const parentelement=document.getElementById('users')
     const childelement=document.createElement('li')
-    childelement.textContent=obj.expenseamount+ '|' + obj.description +  '|' +obj.category
+    childelement.textContent=expense.expenseamount+ '|' + expense.description +  '|' +expense.Category
     const deletebtn=document.createElement('button')
     const editbtn=document.createElement('button')
 
-    deletebtn.type='button'
-    editbtn.type='button'
+    
+    
     deletebtn.textContent='delete'
     editbtn.textContent='edit'
-    deletebtn.style.fontFamily='Arial, Helvetica, sans-serif'
-    deletebtn.style.backgroundColor='red'
-    deletebtn.style.padding='10px 20px'; 
-    deletebtn.style.float='right'
-    editbtn.style.backgroundColor='blue' 
-    editbtn.style.padding='10px 20px'
-    editbtn.style.float='right'
-    deletebtn.onclick=()=>{
-        localStorage.removeItem(obj.description)
+
+    deletebtn.onclick=async()=>{
+        try{
+        await axios.delete(`http://localhost:5000/Expense/Delete-expense/${expense.id}`)
+        console.log('expense deleted:',expense)
+        }
+        catch(err){
+            console.log(err)
+        }
         parentelement.removeChild(childelement)
     }
 
-    editbtn.onclick=()=>{
-        localStorage.removeItem(obj.description)
+    editbtn.onclick=async()=>{
+        await axios.put(`http://localhost:5000/Expense/Edit-expense/${expense.id}`)
         parentelement.removeChild(childelement)
-        document.getElementById('name').value=obj.expenseamount
-        document.getElementById('description').value=obj.description
+        document.getElementById('name').value=expense.expenseamount
+        document.getElementById('description').value=expense.description
+        document.getElementById('Category').value=expense.Category
     }
     childelement.appendChild(deletebtn)
     childelement.appendChild(editbtn)
@@ -47,3 +73,5 @@ function showuseronscreen(obj){
     parentelement.appendChild(childelement)
 
 }
+
+window.addEventListener('DOMContentLoaded',refresh)
